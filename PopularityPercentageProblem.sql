3eb096e1844b3131349c15820d1aba5b7634ea35
lucapiuri <71853938+lucapiuri@users.noreply.github.com>
2024-12-17 17:42:41 +1100

    Update PopularityPercentageProblem.sql

--- PopularityPercentageProblem.sql
+++ PopularityPercentageProblem.sql
@@ -26,7 +26,7 @@ VALUES
 
 
 -- -------------------------------------------------------------------------------------------------------------------------
--- PROBLEM:
+-- THE CHALLANGE:
 -- Find the popularity percentage for each user on Facebook. 
 -- The popularity percentage is defined as the total number of friends the user has 
 -- divided by the total number of users on the platform, then converted into a percentage by multiplying by 100.
