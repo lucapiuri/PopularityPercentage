6c7bfeb7a33af5447b42d558c2c83e44e067c769
lucapiuri <71853938+lucapiuri@users.noreply.github.com>
2024-12-18 07:58:56 +1100

    Update and rename PopularityPercentageProblem.sql to PopularityPercentage.sql

--- PopularityPercentage.sql
+++ PopularityPercentage.sql
@@ -1,3 +1,30 @@
+-- # PopularityPercentage
+-- Solving popularity percentage problem 
+
+--  Using SQL to solve a problem with a simplified facebook_friends dataset
+
+-- THE CHALLENGE:
+-- Find the popularity percentage for each user on Meta/Facebook. The popularity percentage is defined as the total number of friends 
+-- the user has divided by the total number of users on the platform, then converted into a percentage by multiplying by 100.
+
+-- Output each user along with their popularity percentage. Order records in ascending order by user id.
+
+-- The 'user1' and 'user2' column are pairs of friends.
+
+-- user1	    user2
+-- 2	      1
+-- 1	      3
+-- 4	      1
+-- 1	      5
+-- 1	      6
+-- 2	      6
+-- 7	      2
+-- 8	      3
+-- 3	      9
+
+
+
+
 -- ------------------------------------------------------------------
 --              Creating the database and the table:
 -- ------------------------------------------------------------------
